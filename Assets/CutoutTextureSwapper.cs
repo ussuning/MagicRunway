b4daedf1c92a3bd4a54be8527e71a6b962033a81
Henry Ho <henry.ho@ussuning.com>
2018-06-28 16:34:17 -0700

    CutoutTextureSwapper created to combine alphamap with base skin texture (created temporary material with temporary texture)

---
+++ Assets/CutoutTextureSwapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutoutTextureSwapper : MonoBehaviour {
+
+    public Texture2D alphaMap;
+	// Use this for initialization
+	void Start () {
+        Material mat = GetComponent<Renderer>().sharedMaterial;
+        Texture2D srcTex = mat.mainTexture as Texture2D;
+        Color [] srcPixels = srcTex.GetPixels();
+        Color[] alphaPixels = alphaMap.GetPixels();
+
+        if (alphaPixels.Length != srcPixels.Length) {
+            Debug.LogError("Mismatching texture sizes! This won't work!");
+            return;
+        }
+
+        for (int i = 0; i < srcPixels.Length; i++)
+        {
+            srcPixels[i].a = alphaPixels[i].r;
+        }
+
+        // 
+        Texture2D neoTex = new Texture2D(mat.mainTexture.width, mat.mainTexture.height);
+        neoTex.SetPixels(srcPixels);
+        neoTex.Apply();
+
+        Material neoMat = new Material(mat);
+        neoMat.mainTexture = neoTex;
+        GetComponent<Renderer>().sharedMaterial = neoMat;
+
+	}
+}
