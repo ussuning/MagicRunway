c474b50f5a3d9a3221577c58d7b01e4eea64ee49
Henry Ho <henry.ho@ussuning.com>
2018-07-03 17:19:49 -0700

    Runway scene

--- Assets/Scripts/Cloth/AnimationEventHandler.cs
+++ Assets/Scripts/Cloth/AnimationEventHandler.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 using Obi;
 
 public class AnimationEventHandler : MonoBehaviour {
-
     ObiCloth[] cloths = null;
+
+    static float baseVelocity = 1.25f;
+    float velocity = 0;
+    float turnRate = 0;
+    float turnDuration = 0;
 
 
     public void ResetCloth() {
@@ -15,5 +19,52 @@ public class AnimationEventHandler : MonoBehaviour {
         }
         foreach (ObiCloth cloth in cloths)
             cloth.ResetActor();
+    }
+
+    public void IdleStartWalk() {
+        Debug.Log("StartWalk");
+        GetComponent<Animator>().SetTrigger("Start");
+        turnRate = -0.25f;
+        turnDuration = 4.0f;
+    }
+
+    public void IdleToWalkStart(){
+        velocity = baseVelocity;
+    }
+
+    public void WalkStart()
+    {
+        velocity = baseVelocity;
+    }
+
+    public void IdleStart() {
+        velocity = 0;
+    }
+
+    public void IdleToTwistL() {
+        velocity = 0;
+    }
+
+    public void Turn180End() {
+        
+    }
+
+    private void Update()
+    {
+        if (turnDuration != 0)
+        {
+            transform.RotateAround(transform.position, Vector3.up, turnRate * 90.0f * Time.deltaTime);
+            turnDuration = Mathf.Clamp(turnDuration - Time.deltaTime, 0, float.MaxValue);
+            if (turnDuration == 0) {
+                // Walk towards runway;
+                transform.LookAt(GameObject.Find("RunwayEnd").transform);
+                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // only keep y rotation;
+            }
+        }
+        
+        if (velocity != 0)
+            transform.position += transform.forward * velocity * Time.deltaTime; 
+
+
     }
 }
