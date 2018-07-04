2c61d37d5adecc1b70cc0ff659fc7c31f6b75bd6
Henry Ho <henry.ho@ussuning.com>
2018-07-03 18:41:30 -0700

    Runway set up

--- Assets/Scripts/Cloth/AnimationEventHandler.cs
+++ Assets/Scripts/Cloth/AnimationEventHandler.cs
@@ -11,6 +11,11 @@ public class AnimationEventHandler : MonoBehaviour {
     float turnRate = 0;
     float turnDuration = 0;
 
+    public void Reset()
+    {
+        ResetCloth();
+        velocity = turnRate = turnDuration = 0;
+    }
 
     public void ResetCloth() {
         Debug.Log("ResetCloth()");
@@ -24,8 +29,8 @@ public class AnimationEventHandler : MonoBehaviour {
     public void IdleStartWalk() {
         Debug.Log("StartWalk");
         GetComponent<Animator>().SetTrigger("Start");
-        turnRate = -0.25f;
-        turnDuration = 4.0f;
+        turnRate = -0.5f;
+        turnDuration = 2.0f;
     }
 
     public void IdleToWalkStart(){
@@ -46,7 +51,14 @@ public class AnimationEventHandler : MonoBehaviour {
     }
 
     public void Turn180End() {
-        
+        faceTransform2D("RunwayExit");
+        velocity = baseVelocity;
+    }
+
+    public void ExitRunway() {
+        transform.LookAt(new Vector3(999, 0, 0)); // face +X
+        turnRate = -0.5f;
+        turnDuration = 2.0f;
     }
 
     private void Update()
@@ -56,15 +68,28 @@ public class AnimationEventHandler : MonoBehaviour {
             transform.RotateAround(transform.position, Vector3.up, turnRate * 90.0f * Time.deltaTime);
             turnDuration = Mathf.Clamp(turnDuration - Time.deltaTime, 0, float.MaxValue);
             if (turnDuration == 0) {
-                // Walk towards runway;
-                transform.LookAt(GameObject.Find("RunwayEnd").transform);
-                transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // only keep y rotation;
+                if (GetComponent<ModelColliderTrigger>().colliderTriggered.ContainsKey("RunwayEnd") == false)
+                {
+                    // Walk towards runway;
+                    faceTransform2D("RunwayEnd");
+                }
             }
         }
         
         if (velocity != 0)
             transform.position += transform.forward * velocity * Time.deltaTime; 
+        
 
+    }
 
+    public void faceTransform2D(string tName) {
+        Transform target = GameObject.Find(tName).transform;
+        transform.LookAt(target);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // only keep y rotation;
+    }
+
+    public void faceWorldPos2D(Vector3 worldPos) {
+        transform.LookAt(worldPos);
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0); // only keep y rotation;
     }
 }
