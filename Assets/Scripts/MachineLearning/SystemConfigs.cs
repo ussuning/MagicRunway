b09041e73739fca4c138183964eea7e2c9898172
Tai <tai.wu@ussuning.com>
2018-07-24 14:16:10 -0700

    new ML trained data for male poses

--- Assets/Scripts/MachineLearning/SystemConfigs.cs
+++ Assets/Scripts/MachineLearning/SystemConfigs.cs
@@ -7,6 +7,7 @@ public class SystemConfigs
     public static bool CollectUserRotation = false;
     public static bool CollectJointDirectionData = false;
     public static bool CollectJointOrientationData = true;
+    /*
     public static KinectInterop.JointType[] DetectedJoints = {
         KinectInterop.JointType.SpineBase, KinectInterop.JointType.SpineMid, KinectInterop.JointType.Neck, KinectInterop.JointType.Head,
         KinectInterop.JointType.ShoulderLeft, KinectInterop.JointType.ElbowLeft, KinectInterop.JointType.WristLeft, KinectInterop.JointType.HandLeft,
@@ -14,6 +15,19 @@ public class SystemConfigs
         KinectInterop.JointType.HipLeft, KinectInterop.JointType.KneeLeft, KinectInterop.JointType.AnkleLeft, KinectInterop.JointType.FootLeft,
         KinectInterop.JointType.HipRight, KinectInterop.JointType.KneeRight, KinectInterop.JointType.AnkleRight, KinectInterop.JointType.FootRight,
         KinectInterop.JointType.SpineShoulder, KinectInterop.JointType.HandTipLeft, KinectInterop.JointType.HandTipRight
+    };
+    */
+    public static KinectInterop.JointType[] DetectedJoints = {
+        /*KinectInterop.JointType.SpineBase, KinectInterop.JointType.SpineMid, KinectInterop.JointType.Neck, KinectInterop.JointType.Head,*/
+        KinectInterop.JointType.ShoulderLeft, KinectInterop.JointType.ElbowLeft,
+        /*KinectInterop.JointType.WristLeft, KinectInterop.JointType.HandLeft,*/
+        KinectInterop.JointType.ShoulderRight, KinectInterop.JointType.ElbowRight,
+        /*KinectInterop.JointType.WristRight, KinectInterop.JointType.HandRight,*/
+        KinectInterop.JointType.HipLeft, KinectInterop.JointType.KneeLeft,
+        /*KinectInterop.JointType.AnkleLeft, KinectInterop.JointType.FootLeft,*/
+        KinectInterop.JointType.HipRight, KinectInterop.JointType.KneeRight,
+        /*KinectInterop.JointType.AnkleRight, KinectInterop.JointType.FootRight,*/
+        /*KinectInterop.JointType.SpineShoulder, KinectInterop.JointType.HandTipLeft, KinectInterop.JointType.HandTipRight*/
     };
 
     public static float PoseEstimationTimeFrame = 0.1f; //Time between each pose estimation (to remove outlier estimations)
