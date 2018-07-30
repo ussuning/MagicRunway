273bf2ca4376d7a708f17de8c166a972fe4e5a8d
ussuning <administrator@ussuning.com>
2018-07-30 15:23:27 -0700

    --added stickman posing animations (1-7)

---
+++ Assets/Scripts/Posing/StickmanPosing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickmanPosing : MonoBehaviour {
+
+    private Animator anim;
+
+    void Awake ()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        EventMsgDispatcher.Instance.registerEvent(EventDef.New_Pose_Generated, OnNewPoseGenerated);
+    }
+
+    void OnDisable()
+    {
+        EventMsgDispatcher.Instance.unRegisterEvent(EventDef.New_Pose_Generated, OnNewPoseGenerated);
+    }
+
+    public void OnNewPoseGenerated(object param, object paramEx)
+    {
+        int poseID = (int)param;
+        if (anim)
+            anim.SetInteger("pose", poseID);
+    }
+}
