00eff9d57015b83ab688fbe0b058699dd9eeab40
Bill Shao <bill.shao@ussuning.com>
2018-08-03 15:45:27 -0700

    update on animation

--- Assets/Scripts/Managers/ModelAnimationManager.cs
+++ Assets/Scripts/Managers/ModelAnimationManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ModelAnimationManager : MonoBehaviour {
-    public static List<string> femalePoses = new List<string>(new string[] { "roundabout" });
+    public static List<string> femalePoses = new List<string>(new string[] { "women_walking_01","women_walking_02" });
     public static List<string> malePoses = new List<string>(new string[] { "basic" });
    
     public static string GetPoseAnimation(string sex, bool random = true)
