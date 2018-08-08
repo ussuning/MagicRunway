e4553d390574dfea29007d56b85e6e7a4642faf2
Henry Ho <henry.ho@ussuning.com>
2018-08-08 15:08:30 -0700

    Depth-to-Color Camera conversion calibrated.

---
+++ Assets/Scripts/CameraConversion/camProjectionMatrixManipulator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camProjectionMatrixManipulator : MonoBehaviour {
+    public Matrix4x4 projectionMatrix = Matrix4x4.identity;
+    public Matrix4x4 offsetMatrix = Matrix4x4.zero;
+    protected Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+        projectionMatrix = cam.projectionMatrix;
+        for (int row = 0; row < 4; row++)
+        {
+            Vector4 values = projectionMatrix.GetRow(row);
+            Vector4 offsets = offsetMatrix.GetRow(row);
+            values += offsets;
+            projectionMatrix.SetRow(row, values);
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (projectionMatrix != cam.projectionMatrix)
+        {
+            cam.projectionMatrix = projectionMatrix;
+        }
+	}
+}
